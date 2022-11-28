363eb3a8e740e13f0524cb7733eac51daa5e175b
Massimiliano Collavo <mcollavo@gmail.com>
2022-11-28 10:40:08 +0100

    Update ImageToSound.java

--- application.macosx64/source/ImageToSound.java
+++ application.macosx64/source/ImageToSound.java
@@ -19,9 +19,6 @@ public class ImageToSound extends PApplet {
 
 /*------------------------------------------------------------------
   
-                         
-                           
-  
                              Image To sound
                              
 Trasforma immagine in suono e disegna una spirale in base alla nota con applicato rumore di perlin
@@ -30,11 +27,6 @@ Istruzioni:
 - Premere il pulsante del mouse per acquisire il background
 - Premere i tasti '1' o '2' per attivare il teletrasporto
 -------------------------------------------------------------------*/
-
-
-
-
-
 
 // schermo dimensione
 final int screenWidth = 1024;
@@ -55,28 +47,22 @@ int[] midiSequence = {
 14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,77,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107
 };
 
-
 */
 
 Capture video; // dichiarazione variabile Capture
-
 imageToSound sound; //Dichiarazione varibile di tipo ImagetoSound
-
 
 int numNote = 30;
 int[] midiSequence = new int[numNote];
 
 /* Disegno */
-
 int x,y;
 int curvePointX = 0;
 int curvePointY = 0;
 int pointCount = 1;
 float diffusion = 50;
 
-
-/* 
-* Oscilaltore ed envelope */
+/* Oscilaltore ed envelope */
 
 TriOsc triOsc;
 Env env;
@@ -85,13 +71,9 @@ int duration = 200; // Setta la durata fra le note
 int trigger = 10; // Setta il trigger delle note
 int inote = 0; // indice per contare il numero di note
 
-
 PImage prev; // immagine precedente
 
-
 public void setup() {
- 
-
  video = new Capture(this, screenWidth, screenHeight); //creazione oggetto di tipo Capture
  video.start(); //cattura da webcam abilitata
 
@@ -99,25 +81,20 @@ public void setup() {
  triOsc = new TriOsc(this);
  env = new Env(this);
  
- 
- ///Creazione oggetto Image sound. Si noti il costruttore dopo l'istruzione new
-   sound = new imageToSound( video,video, threshold);
-
+  //Creazione oggetto Image sound. Si noti il costruttore dopo l'istruzione new
+  sound = new imageToSound( video,video, threshold);
 }
-
 
 public void draw() {
   //frameRate(30);
   background(0xff000000);
   smooth();
   //noFill();
-  
 
   if (video.available()) {
 
    prev = video.copy(); // copia immagine video in prev(vecchio frame)
    video.read(); // leggi nuovo frame
-
 
    int threshold = 20; /// valore di soglia
    //int thres=int(map(mouseX,0, width,0, 255)); /// valore di soglia
@@ -146,30 +123,20 @@ public void draw() {
                 
             // Aggiorno il trigger in base alla durata e velocit\u00e0
             trigger = millis() + duration;
-                  
-
-            
+                 
             // disegno spirale
 
-            sound.drawSound(midiSequence[inote],prev);
-            
-            
-            
-            
+            sound.drawSound(midiSequence[inote],prev); 
+
           inote ++;
           if (inote == numNote) inote=0;
   } /// fine suono note
        
    /* ########## */      
 
-
-
-  
-
   } //end video avaiable
 
  } //end draw
-
 
 
 /// fermo o faccio ripartire il loop
@@ -179,7 +146,6 @@ public void keyReleased() {
  if (key == 'q' || key == 'S') noLoop();
  if (key == 'w' || key == 'W') loop();
  
-
 }
   class imageToSound {
     
@@ -194,7 +160,6 @@ public void keyReleased() {
 
     println("Oggetto ImageSound inizializzato!!!");
    }
-
 
    /*
     * questa funziona controlla che ci siano veriazioni della immagine e mappa le note
@@ -224,7 +189,6 @@ public void keyReleased() {
      // int lineSum1 = 0;
 
      for (int x = 0; x < src.width; x++) {
-
 
       int index = x + y * src.width; //conversione da punto 2D a indice array 1D di img
 
@@ -273,8 +237,6 @@ public void keyReleased() {
      src.updatePixels(); //apporta modifiche di qui sopra ad image
      updatePixels();
     }
-
-
    }
    
    
@@ -293,17 +255,11 @@ public void keyReleased() {
       env.play(triOsc, attackTime, sustainTime, sustainLevel, releaseTime);
 
    }
-
-   
     //  disegna una spirale
    //varianza degli assi
    //float stepa, stepb; // incrementi degli assi
 
    public void drawSound(int note, PImage prev) {
-     
-
- 
-
     // prima linea
     //int pixelIndex = ((video.width - 1 - x) + y * video.width);
     // random
@@ -392,10 +348,7 @@ public void keyReleased() {
        noi = noi + 0.05f;
     }
    }
-   
-   
   }
-  
   
 /*
     questa funziona calcola la rispettiva frequanza di una nota midi
